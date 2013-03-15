6215d98f3cf73644416dda3e921fec03a056b96d
John-Michael Velasquez <johnmichael@wednesday-london.com>
2013-03-15 16:42:25 +0000

    implemented changes to recursive file function and index.php
    Signed-off-by: John-Michael Velasquez <johnmichael@wednesday-london.com>

--- finder/recursivedirsearch.php
+++ finder/recursivedirsearch.php
@@ -1,7 +1,5 @@
 <?php
 
-print_r(makeULLI(readDirR("../FILES")));
-
 function readDirR($dir = "../FILES") {
     $listing = opendir($dir);
     $return = array();
@@ -40,31 +38,6 @@ function makeULLI($array) {
     }
 
     $return .= "</ol>";
-
-    return $return;
-}
-
-//If you wanted to output links, this is the quickest solution I came up with (albeit dirty)
-
-function readDirR($dir = "./") {
-    $listing = opendir($dir);
-    $return = array();
-    while (($entry = readdir($listing)) !== false) {
-        if ($entry != "." && $entry != "..") {
-            $dir = preg_replace("/^(.*)(\/)+$/", "$1", $dir);
-            $item = $dir . "/" . $entry;
-            $link = '<a href="' . $dir . "/" . $entry . '">' . $entry . '</a>';
-            if (is_file($item)) {
-                $return[] = $link;
-            } elseif (is_dir($item)) {
-                $return[$link] = readDirR($item);
-            } else {
-                
-            }
-        } else {
-            
-        }
-    }
 
     return $return;
 }
--- index.php
+++ index.php
@@ -17,11 +17,16 @@
 
             
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 include "finder/recursivedirsearch.php";
-$dir = "../FILES";
-readDirR($dir);
+$dir = "FILES";
+print_r(makeULLI(readDirR($dir)));
+
+//test('WHYYY');
 ?>
-        
  
 </body>
 </html>
