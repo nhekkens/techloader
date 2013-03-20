f86b2d6b22b529c79a42c9a0dd8ce810512f8466
Nicolay <nicolay@wednesday-london.com>
2013-03-20 09:41:36 +0000

    Update
    Signed-off-by: Nicolay <nicolay@wednesday-london.com>

--- index.php
+++ index.php
@@ -3,43 +3,69 @@
 <head>
     <meta charset="utf-8">
     <title>Tech Loader 9000</title>
-    <link rel="stylesheet" type="text/css" href="uploader/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/jqueryFileTree.css">
     <script src="uploader/uploader.js"></script>
     <script src="http://code.jquery.com/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="finder/jquery.columnview.js" type="text/javascript" charset="utf-8"></script>
+    <script src="finder/jqueryFileTree.js" type="text/javascript" charset="utf-8"></script>
 </head>
 
 <body onload="new uploader('drop', 'status', 'uploader/uploader.php', 'list');">
-	<div id="box">
-		<div id="status">UPLOAD AREA</div>
-                <div id="drop"><img src="uploader/upload2.gif"></div>
-		<p>DRAG & DROP</p>
-	</div>
+
+<?php
+/*
+Our php.ini contains the following settings:
+
+display_errors = On
+register_globals = Off
+post_max_size = 8M
+*/
+
+echo 'display_errors = ' . ini_get('display_errors') . "\n";
+echo 'register_globals = ' . ini_get('register_globals') . "\n";
+echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+echo 'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'));
+
+function return_bytes($val) {
+    $val = trim($val);
+    $last = strtolower($val[strlen($val)-1]);
+    switch($last) {
+        // The 'G' modifier is available since PHP 5.1.0
+        case 'g':
+            $val *= 1024;
+        case 'm':
+            $val *= 1024;
+        case 'k':
+            $val *= 1024;
+    }
+
+    return $val;
+}
+
+?>
 	
     
-    
-    
-    
-    
+    <div id="box">
+        <div id="status">UPLOAD AREA</div>
+        <div id="drop"><img src="uploader/upload2.gif"></div>
+        <p>DRAG & DROP</p>
+    </div>
+ 
+<div id="finder">
+     <script>
+    $(document).ready( function() {
+    $('#finder').fileTree({ 
+        root: 'FILES/' }, 
+        function(file) {
+        alert(file);
+        });
+    });   
+    </script>
+</<div>        
 
-<div id="dirlist" style="display:none">    
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-include "finder/recursivedirsearch.php";
-$dir = "FILES";
-print_r(makeULLI(readDirR($dir)));
-?>
-</div>
-    <div id="finder">
-    </div>        
     
-<!--    
- <script type="text/javascript">
-      $("div#dirlist > ul:first").clone(true).attr("id", "list1").appendTo("div#finder");
-      $("ul#list1").columnview();
-</script>
     
-    -->
+   
 </body>
 </html>
