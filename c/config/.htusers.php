c6df08d73018e96df4a5c2c8c43b272418107752
Nicolay <nicolay@wednesday-london.com>
2013-03-22 13:22:10 +0000

    added a pre made browser
    Signed-off-by: Nicolay <nicolay@wednesday-london.com>

---
+++ c/config/.htusers.php
@@ -0,0 +1,8 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$akRXegjU654gtjfWoqVbQu.nx6G9I.uPo7exJ4owyctRpWxuFxUhq','/Users/nicolayhekkens/sites/techloader/','http://localhost','1','','7',1),
+	array('guest','$2a$08$IeMchErS118OMnjRURHOV.b0zZol.wa4TWJbG4xKuFs.ilxLSwDZa','/Users/nicolayhekkens/sites/techloader/','http://localhost','0','','0',1),
+); 
+?>
