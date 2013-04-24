eb7245c74a7a26c78670d9050070aa5316334c5a
Nicolay <nicolay@wednesday-london.com>
2013-04-24 10:33:45 +0100

    big change
    Signed-off-by: Nicolay <nicolay@wednesday-london.com>

---
+++ d/index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Wednesday</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--bootstrap-->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+        <!--isotope-->
+        <link rel="stylesheet" href="isotope/css/style.css">
+        <script src="isotope/js/jquery-1.7.1.min.js"></script>
+        <script src="isotope/js/jquery.isotope.js"></script>
+        <script src="isotope/js/jquery.isotope.min.js"></script>        
+        <script src="isotope/js/fake-element.js"></script>
+        <script src="isotope/js/jquery.ba-bbq.min.js"></script>
+        <script src="isotope/js/jquery.infinitescroll.min.js"></script>
+        <script src="isotope/js/make-big-graph-projects.js"></script>
+        <!--uploader-->
+        <script src="uploader/uploader.js"></script>
+    </head>
+    
+    <body  onload="new uploader('drop', 'status', 'uploader/uploader.php', 'list');">
+        
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span3">
+                       <section id="options" class="clearfix">
+                            <ul id="filters">
+                                <li><a href="#" data-filter="*">show all</a></li>
+                                <li><a href="#" data-filter=".image">Images</a></li>
+                                <li><a href="#" data-filter=".video">Videos</a></li>
+                                <li><a href="#" data-filter=".keynote">Presentations</a></li>
+                                <li><a href="#" data-filter=".photoshop">Photoshop</a></li>
+                                <li><a href="#" data-filter=".word">Docs</a></li>
+                            </ul>
+                       </section>
+                    
+                       <div id="box">
+                            <div id="status"></div>
+                            <div id="drop"><img src="uploader/upload2.png"></div>
+                       </div>
+                </div>
+         
+                <div class="span9 isotope">
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">112kb</p>
+                     </div>
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">788kb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">233kb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">127kb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">12kb</p>
+                     </div> 
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">100mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">652kb</p>
+                     </div>  
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">532mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">912kb</p>
+                     </div>
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">12kb</p>
+                     </div> 
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">100mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">652kb</p>
+                     </div>  
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">532mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">912kb</p>
+                     </div>
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">12kb</p>
+                     </div> 
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">100mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">652kb</p>
+                     </div>  
+
+                     <div class="element video" data-symbol="H" data-category="other">
+                       <p class="number">video</p>
+                       <p class="weight">532mb</p>
+                     </div>  
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element image" data-symbol="H" data-category="other">
+                       <p class="number">Image</p>
+                       <p class="weight">912kb</p>
+                     </div>
+
+                     <div class="element keynote" data-symbol="H" data-category="other">
+                       <p class="number">keynote</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element photoshop" data-symbol="H" data-category="other">
+                       <p class="number">photoshop</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                     <div class="element word" data-symbol="H" data-category="other">
+                       <p class="number">word</p>
+                       <p class="weight">2.1mb</p>
+                     </div>  
+
+                </div>
+            </div>     
+        </div>
+        
+<!--Uploader PHP-->
+<?php
+//echo 'display_errors = ' . ini_get('display_errors') . "\n";
+//echo 'register_globals = ' . ini_get('register_globals') . "\n";
+//echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+//echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+//echo 'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'));
+
+function return_bytes($val) 
+{
+    $val = trim($val);
+    $last = strtolower($val[strlen($val)-1]);
+    switch($last) 
+    {
+        case 'g':
+            $val *= 1024;
+        case 'm':
+            $val *= 1024;
+        case 'k':
+            $val *= 1024;
+    }
+
+    return $val;
+}
+?>     
+<!--Isotope Script-->
+<script>
+          $(document).ready(function() {
+            
+            var $container = $('.isotope');
+
+            $container.isotope
+            ({
+
+            });
+          
+            console.log('$container',$container);
+            // filter items when filter link is clicked
+            $('#filters a').on('click',function(){
+                console.log('click');
+              var selector = $(this).attr('data-filter');
+              $container.isotope({ filter: selector });
+              return false;
+            });
+          });
+</script>          
+</body>
+</html>
